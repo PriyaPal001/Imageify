904bbfc62794fc90e92980abd00705d6774e4434
Priya Pal <vu3t1s2021032@pvppcoe.ac.in>
2025-03-27 06:32:09 +0530

    update

--- src/lib/components/ui/dropdown-menu/dropdown-menu-radio-item.svelte
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-radio-item.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { DropdownMenu as DropdownMenuPrimitive } from "bits-ui";
-	import {DotFilled} from "lucide-svelte";
+	// import {DotFilled} from "lucide-svelte";
 	import { cn } from "$lib/utils.js";
 
 	type $$Props = DropdownMenuPrimitive.RadioItemProps;
@@ -28,7 +28,7 @@
 >
 	<span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
 		<DropdownMenuPrimitive.RadioIndicator>
-			<DotFilled class="h-4 w-4 fill-current" />
+			<!-- <DotFilled class="h-4 w-4 fill-current" /> -->
 		</DropdownMenuPrimitive.RadioIndicator>
 	</span>
 	<slot />
